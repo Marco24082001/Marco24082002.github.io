a0852b2e6bb169e36d013b5df1f8607d838614bf
Marco2408 <vivt.hoianmates@gmail.com>
2021-06-09 17:53:58 +0700

    chatBot-Simsimi

--- S-group/Python/Exercise/exercise4/main.py
+++ S-group/Python/Exercise/exercise4/main.py
@@ -57,10 +57,10 @@ class Chatbot:
         while True:
             try:
                 self.speech.Listen()
+                print(f"\033[95mYou: {self.speech.message}")
                 payload = "{\n\t\"utext\": \"" + self.speech.message + "\", \n\t\"lang\": \"vi\" \n}"
                 response = requests.post(self.url, data=payload.encode(), headers=self.headers)
                 response = json.loads(response.text)
-                print(f"\033[95mYou: {self.speech.message}")
                 print(f"\033[91mSimi : {response['atext']}")
                 self.read.Read(response['atext'])
             except Exception as ex: 
