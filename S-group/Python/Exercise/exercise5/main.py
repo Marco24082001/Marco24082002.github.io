c8f2afab7399e300eee4a45b29ec94eb0a834095
Marco2408 <vivt.hoianmates@gmail.com>
2021-06-26 23:55:32 +0700

    done

--- S-group/Python/Exercise/exercise5/main.py
+++ S-group/Python/Exercise/exercise5/main.py
@@ -1,6 +1,6 @@
 import numpy as np
 import cv2 
-
+image = cv2.imread("image.png", 1)
 def grayScale(img):
     # img[:] = np.sum(img,axis=-1,keepdims=1)/3
     formula = [0.07, 0.72, 0.21]
@@ -12,9 +12,16 @@ def Black_White(img):
     img = np.where(img >= 95, img, 0)
     return img
 
-image = cv2.imread("image.png", 1)
+
 cv2.imshow("Origin", image)
 cv2.imshow("Grayscale", grayScale(image))
 cv2.imshow("Black & White", Black_White(grayScale(image)))
 
 cv2.waitKey(10000)  
+hist = np.zeros((256), np.uint8)
+[a, b] = image.shape[:2]
+print(a)
+print(b)
+print(image[100][15])
+hist[(2,4)] += 1
+print(hist)
---
+++ S-group/Python/Exercise/exercise6/main.py
@@ -0,0 +1,41 @@
+import cv2
+import numpy as np 
+
+def crop_Plate(img):
+    imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(imgray, 125, 255, cv2.THRESH_BINARY_INV)
+    contours, hierarchy = cv2.findContours(image=thresh, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_SIMPLE)                                    
+    cv2.drawContours(image=imgray, contours=contours, contourIdx=-1, color=(0, 255, 0), thickness=3, lineType=cv2.LINE_AA)
+
+    listcons = []
+    for cnt in contours:
+        listcons.append(cv2.contourArea(cnt))
+    listcons.sort(reverse=True)
+    for cnt in contours:
+        (x,y,w,h) = cv2.boundingRect(cnt)
+        if  cv2.contourArea(cnt)==listcons[1] :
+            cv2.rectangle(img, (x,y), (x+w,y+h), (223,0,41), 2)
+            crop_img = img[y:y+h, x:x+w]
+    return crop_img
+
+def crop_Number(img):
+    imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    thresh = cv2.threshold(imgray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours=sorted(contours,key=cv2.contourArea,reverse=True)
+    num = 0
+    for contour in contours:
+        area =cv2.contourArea(contour)
+        if area <= 3500 and area >= 900:
+            x,y,w,h = cv2.boundingRect(contour)
+            number = img[y:y+h, x:x+w]
+            img=cv2.rectangle(img, (x,y), (x+w,y+h), (0,0,255), 2)
+            cv2.imwrite('./number/Num_{}.png'.format(num), number)
+            num += 1
+
+img = cv2.imread('./image/1.jpg')
+crop_plate = crop_Plate(img)
+crop_Number(crop_plate)
+cv2.imshow('crop', crop_plate)
+cv2.imwrite('./number/crop.png',crop_plate)
+cv2.waitKey(5000)
